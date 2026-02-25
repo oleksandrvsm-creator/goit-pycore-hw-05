c2cdd233941704847eef17f3c325b6229f877038
Oleksandr <oleksandr.v.sm@gmail.com>
2026-02-26 01:57:18 +0200

    patern_changed

--- task_2/main.py
+++ task_2/main.py
@@ -3,7 +3,7 @@ import re
 
 
 def generator_numbers(text: str) -> Generator[float, None, None]:
-    pattern = r'\b[-+]?\d+\.?\d+\b'
+    pattern = r'\b\d+(?:\.\d+)?\b'
     for match in re.finditer(pattern, text):
         yield float(match.group())
 
